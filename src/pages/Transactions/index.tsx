6dd044e0f4fa6b14875772209f6e93feef40b268
jvmoreirad <jvitormoreirad@hotmail.com>
2023-01-09 16:59:58 -0300

    feat: estrutura visual

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,41 @@
+import { Header } from "../../components/Header/indext";
+import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighlight, TransactionContainer, TransactionTable } from "./styles";
+
+export function Transaction() {
+    return (
+        <div>
+            <Header />
+            <Summary />
+            <TransactionContainer>
+                <SearchForm />
+                <TransactionTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de um site</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Hambúrguer</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    - R$ 59,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>10/04/2022</td>
+                        </tr>
+                    </tbody>
+                </TransactionTable>
+            </TransactionContainer>
+
+        </div>
+    )
+}
